3d762d49971135859be2297793cb9b4d511f75d9
shirase-aoi <ieee0824@ezweb.ne.jp>
2015-08-12 11:31:34 +0900

    投稿テスト

--- cmd/bot.go
+++ cmd/bot.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/shirase-aoi/twitterBOT"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/shirase-aoi/twitterBOT"
+)
 
 func main() {
-	bot.RandomPost()
+	rand.Seed(time.Now().UnixNano())
+	postTime := time.Now().Unix()
+	postTime = 0
+	for {
+		if time.Now().Unix()-postTime > (30*60)+time.Now().Unix()%10 {
+			bot.RandomPost()
+			fmt.Println("post")
+		}
+		time.Sleep(time.Minute * 1)
+	}
 }
